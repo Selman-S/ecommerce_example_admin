21df5a36b2deed5cefd155a830116019d1932797
selman <gunduzgece546@gmail.com>
2024-05-08 18:17:23 +0300

    Refactor dashboard page and API routes

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -1,9 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import Collection from "@/lib/models/Collection";
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs/server";
 
-import { connectToDB } from "@/lib/mongoDB";
-import Product from "@/lib/models/Product";
-import Collection from "@/lib/models/Collection";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
   req: NextRequest,
@@ -12,7 +12,10 @@ export const GET = async (
   try {
     await connectToDB();
 
-    const product = await Product.findById(params.productId).populate({path:"collections",model:Collection});
+    const product = await Product.findById(params.productId).populate({
+      path: "collections",
+      model: Collection,
+    });
 
     if (!product) {
       return new NextResponse(
@@ -20,11 +23,17 @@ export const GET = async (
         { status: 404 }
       );
     }
-
-    return NextResponse.json(product, { status: 200 });
-  } catch (error) {
-    console.log("product_get", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse(JSON.stringify(product), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": `${process.env.ECOMMERCE_STORE_URL}`,
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  } catch (err) {
+    console.log("[productId_GET]", err);
+    return new NextResponse("Internal error", { status: 500 });
   }
 };
 
@@ -34,19 +43,22 @@ export const POST = async (
 ) => {
   try {
     const { userId } = auth();
+
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 403 });
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
     await connectToDB();
 
-    let product = await Product.findById(params.productId);
+    const product = await Product.findById(params.productId);
 
     if (!product) {
-      return new NextResponse("Product not found", { status: 404 });
+      return new NextResponse(
+        JSON.stringify({ message: "Product not found" }),
+        { status: 404 }
+      );
     }
 
-    
-    
     const {
       title,
       description,
@@ -59,46 +71,42 @@ export const POST = async (
       price,
       expense,
     } = await req.json();
-    
+
     if (!title || !description || !media || !category || !price || !expense) {
-      return new NextResponse("Not enough data to create a product", {
+      return new NextResponse("Not enough data to create a new product", {
         status: 400,
       });
     }
-    
-    
-      // added collections
-    
-      const addedCollections = collections.filter((collectionId:string) => {
-        return !product.collections.includes(collectionId);
-      });
 
-      // removed collections
+    const addedCollections = collections.filter(
+      (collectionId: string) => !product.collections.includes(collectionId)
+    );
+    // included in new data, but not included in the previous data
 
-      const removedCollections = product.collections.filter((collectionId:string) => {
-        return !collections.includes(collectionId);
-      });
+    const removedCollections = product.collections.filter(
+      (collectionId: string) => !collections.includes(collectionId)
+    );
+    // included in previous data, but not included in the new data
 
-      await Promise.all([
-        //update collections
-        ...addedCollections.map((collectionId:string) => 
-          Collection.findByIdAndUpdate(collectionId, {
-            $push: { products: product._id },
-          })
-        ),
-      ]);
-      
-      await Promise.all([
-        //update collections
-        ...removedCollections.map((collectionId:string) => 
-           Collection.findByIdAndUpdate(collectionId, {
-            $pull: { products: product._id },
-          })
-          ),
-      ]);
+    // Update collections
+    await Promise.all([
+      // Update added collections with this product
+      ...addedCollections.map((collectionId: string) =>
+        Collection.findByIdAndUpdate(collectionId, {
+          $push: { products: product._id },
+        })
+      ),
 
-      // update product
-    const updateProduct = await Product.findByIdAndUpdate(
+      // Update removed collections without this product
+      ...removedCollections.map((collectionId: string) =>
+        Collection.findByIdAndUpdate(collectionId, {
+          $pull: { products: product._id },
+        })
+      ),
+    ]);
+
+    // Update product
+    const updatedProduct = await Product.findByIdAndUpdate(
       product._id,
       {
         title,
@@ -113,16 +121,16 @@ export const POST = async (
         expense,
       },
       { new: true }
-    ).populate({path:"collections",model:Collection});
+    ).populate({ path: "collections", model: Collection });
 
-    await updateProduct.save();
+    await updatedProduct.save();
 
-    return NextResponse.json(updateProduct, { status: 200 });
-  } catch (error) {
-    console.log("product_post", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return NextResponse.json(updatedProduct, { status: 200 });
+  } catch (err) {
+    console.log("[productId_POST]", err);
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
 
 export const DELETE = async (
   req: NextRequest,
@@ -130,51 +138,40 @@ export const DELETE = async (
 ) => {
   try {
     const { userId } = auth();
+
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 403 });
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
     await connectToDB();
 
-    let product = await Product.findById(params.productId);
+    const product = await Product.findById(params.productId);
 
     if (!product) {
-      return new NextResponse("Product not found", { status: 404 });
+      return new NextResponse(
+        JSON.stringify({ message: "Product not found" }),
+        { status: 404 }
+      );
     }
 
-    await Product.findByIdAndDelete(params.productId);
+    await Product.findByIdAndDelete(product._id);
 
-
-    // update other collections
+    // Update collections
     await Promise.all(
-      product.collections.map((collectionId:string) =>
+      product.collections.map((collectionId: string) =>
         Collection.findByIdAndUpdate(collectionId, {
           $pull: { products: product._id },
         })
       )
     );
 
-    return new NextResponse("Product deleted", { status: 200 });
-  } catch (error) {
-    console.log("product_delete", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    return new NextResponse(JSON.stringify({ message: "Product deleted" }), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log("[productId_DELETE]", err);
+    return new NextResponse("Internal error", { status: 500 });
   }
 };
-// export const DELETE = async (
-//   req: NextRequest,
-//   { params }: { params: { collectionId: string } }
-// ) => {
-//   try {
-//     const { userId } = auth();
-//     if (!userId) {
-//       return new NextResponse("Unauthorized", { status: 403 });
-//     }
-//     await connectToDB();
 
-//     await Collection.findByIdAndDelete(params.collectionId);
-
-//     return new NextResponse("Collection deleted", { status: 200 });
-//   } catch (error) {
-//     console.log("collection_delete", error);
-//     return new NextResponse("Internal Server Error", { status: 500 });
-//   }
-// };
+export const dynamic = "force-dynamic";
